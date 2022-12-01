a89576fe67da9aa691ca4181c3dff78a3bca4b2b
PavelTalibullin <pavlotalibullin@gmail.com>
2022-12-01 17:14:27 +0300

    practic DOM

---
+++ dom.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// const article = document.getElementById("articleId");
+
+// const menu = document.getElementsByClassName("menu");
+
+// const paragraphs = document.getElementsByTagName('p')
+
+// const paragraph = document.querySelector("p:last-child")
+// const articleId = document.querySelectorAll("#articleId")
+
+const article = document.querySelector("#articleId");
