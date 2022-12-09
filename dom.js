88ee87acc81d4ad04a032797fa48cb98703d6f55
PavelTalibullin <pavlotalibullin@gmail.com>
2022-12-09 21:31:54 +0300

    .

--- dom.js
+++ dom.js
@@ -1,9 +1,126 @@
-'use strict'
+//								Свойства Document
+// document.title = '';
+// document.forms
+// document.body
+// document.head
 
-// const article = document.getElementById("articleId");
 
-const article = document.getElementById('articleId');
+// 							Методы поиска Элемента
+// document.getElementById("")
+// document.getElementsByClassName("")
+// document.getElementsByTagName("")
+// document.querySelector("")
+// document.querySelectorAll("")
 
-// const arrayOfParagraph = article.getElementsByClassName('paragraph');
 
-// const paragraph = document.querySelector('#articleId')   
+//							HTMLCollection и NodeList
+// NodeList
+// HTMLCollection
+// Element
+
+
+//							Свойста связанные с html-атрибутами
+// id
+// element.className = ""
+// element.style.backgroundColor = "beige"
+
+
+// 						Свойства и методы связанные с DOM
+// children
+// parentElement 
+// nextElementSibling и previousElementSibling
+// firstElementChild, lastElementChild
+
+// 						Свойства с информацией о содержимом
+// innerHTML
+// outerHTML
+// textContent
+
+
+//							Свойство classList
+// element.classList.add()
+// element.classList.remove()
+// element.classList.replace()
+// element.classList.toggle()
+
+
+// 												УРОК 2
+// свойство hidden
+
+
+//							Атрибуты и свойства
+// elem.hasAttribute(name)
+// elem.getAttribute(name) 
+// elem.setAttribute(name, value)
+// elem.removeAttribute(name)
+// elem.attributes
+
+
+//							Нестандартные атрибуты, dataset
+
+
+//							Изменение Документа
+//							(Создание элемента)
+//document.createElement(tag)
+//document.createTextNode(text)
+
+//							Методы вставки
+//node.append() 
+//node.prepend()
+//node.before() 
+//node.after()
+//node.replaceWith() 
+
+
+//							insertAdjacentHTML
+//elem.insertAdjacentHTML(where, html)
+//"beforebegin"
+//"afterbegin"
+//"beforeend" 
+//"afterend"
+//(второй параметр)
+
+
+//							Удаление узлов
+// node.remove()
+
+//							Клонированиие узлов: cloneNode
+//elem.cloneNode(true)
+//elem.cloneNode(false)
+
+
+//							Ввод в браузерные события
+//событие
+//click
+//change
+//blur
+//input
+//submit
+//DOMContentLoaded
+//load
+//unload
+//keydown 
+//keyup 
+//mouseover 
+//mouseout 
+//scroll 
+
+
+//								Обработчик событий
+//Обработчик событий
+//									addEventListener
+//event 
+//handler 
+//options (once,capture,passive)
+
+
+//									Удаление обработчика
+//element.removeEventListener(event, handler, [options])
+
+
+//									Объект события(и свойства и методы)
+//	type
+//	target
+//preventDefault()
+//stopPropagation()
+//stopImmediatePropagation()
